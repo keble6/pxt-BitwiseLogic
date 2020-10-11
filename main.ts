d69e2cddee54ea1de312f07a16a4c4836b0204ac
Rob Wilson <rob@rwilson.org.uk>
2020-10-11 22:04:28 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
-basic.forever(function () {
-	
-})
+/* *************** */
+/*We use block="string" to override the block drop-down text */
+enum operator{
+    //% block="&"
+    and,
+    //% block="|"
+    or,
+    //% block="^"
+    xor,
+    //% block="<<"
+    leftShift,
+    //% block=">>"
+    rightShift,
+    //% block=">>>"
+    zeroFillRightShift
+}
+namespace BitwiseLogic {
+
+    
+    //% blockId="bitwise_2arg" block="%a %fn %b"
+    //% weight=98 blockGap=8
+    export function bitwise2arg(a: number, fn: operator, b:number) {
+        switch (fn){
+            case operator.and: return(a & b)
+                break
+            case operator.or: return(a | b)
+                break
+            case operator.xor: return(a ^ b)
+                break
+            case operator.leftShift: return(a << b)
+                break
+            case operator.rightShift: return(a >> b)
+                break
+            case operator.zeroFillRightShift: return(a >>> b)
+                break
+        }
+    }
+        
+    //% blockId="bitwise_not" block="~ %a"
+    //% weight=94 blockGap=8
+    export function bitwiseNot(a: number){
+        return(~a)
+    }
+
+
+}
