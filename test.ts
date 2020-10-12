60d099d7ccf0c9ca65092ea9761288d0d78c663a
Rob Wilson <rob@rwilson.org.uk>
2020-10-12 19:02:51 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,21 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+
+/*** test the NOT function */
+function checkNot(str: string){
+    let a = parseInt(str,10)    //convert to number
+    let jsResult = ~a & 0xff        // convert to 8-bit unsigned result
+    let myResult = BitwiseLogic.bitwiseNot(a) & 0xff
+    //basic.showNumber(jsResult)
+    //basic.showNumber(myResult)
+    if(jsResult == myResult){
+        basic.showString("P")
+    }
+    else {
+        basic.showString("F")
+    }
+}
+basic.showString("~")
+checkNot("a5")
+basic.showString(":")
+checkNot("5a")
+basic.showString(":")
+
